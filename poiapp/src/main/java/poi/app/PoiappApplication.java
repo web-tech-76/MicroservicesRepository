6e7085a39a448681b4f56bf7cadb6f673ac5d795
web-tech-76 <webtec76@gmail.com>
2020-07-02 14:09:10 +0530

    initial commit

---
+++ poiapp/src/main/java/poi/app/PoiappApplication.java
@@ -0,0 +1,13 @@
+package poi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoiappApplication.class, args);
+	}
+
+}
