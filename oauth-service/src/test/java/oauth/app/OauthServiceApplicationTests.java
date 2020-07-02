bb7e030b132bff4c7f645e560adf96bfb6ce7dc2
web-tech-76 <webtec76@gmail.com>
2020-07-02 16:39:24 +0530

    initial commit.

---
+++ oauth-service/src/test/java/oauth/app/OauthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package oauth.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
